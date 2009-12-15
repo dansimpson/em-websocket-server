157dd8c433b9e8edde31c6eef0f2ca3de6b0c407
Jordan Fowler <me@jordanfowler.com>
2009-12-15 00:01:03 -0800

    Working Tic Tac Toe.

--- examples/tictactoe.js
+++ examples/tictactoe.js
@@ -1,14 +1,19 @@
-// Author: Jordan Fowler
-
-var currentGame = null;
+// Author: Jordan Fowler (TheBreeze)
 
 var TicTacToe = new Class({
+  wins: 0,
+  loses: 0,
+  draws: 0,
   methodMap: {
     'queued': 'onQueued',
     'start': 'onStart',
     'turn': 'onTurn',
     'move': 'onMove',
-    'game_over': 'onGameOver'
+    'game_over': 'onGameOver',
+    'win': 'onWin',
+    'loss': 'onLoss',
+    'draw': 'onDraw',
+    'user_count': 'onUserCount'
   },
 
   initialize: function() {
@@ -16,9 +21,11 @@ var TicTacToe = new Class({
       TicTacToe.connection = new WebSocket('ws://192.168.0.2:8000/tictactoe');
     };
 
-    TicTacToe.connection.onopen = this.onJoin.bind(this);
+    TicTacToe.connection.onopen = this.join.bind(this);
     TicTacToe.connection.onmessage = this.onMessage.bind(this);
     TicTacToe.connection.onclose = this.onGameOver.bind(this);
+
+    this.setGameStats();
   },
 
   onMessage: function(event) {
@@ -26,37 +33,96 @@ var TicTacToe = new Class({
 
     console.log('[RCV] ' + command.msg);
 
-    this[this.methodMap[command.msg]].call(this, [command]);
+    this[this.methodMap[command.msg]].call(this, command);
   },
 
-  onJoin: function(event) {
-    console.log('[SENT] join game');
+  message: function(msg, options) {
+    var command = JSON.encode({msg: msg, data: options});
 
-    TicTacToe.connection.send(JSON.encode({msg: 'join'}));
+    console.log('[SENT] ' + msg);
+
+    TicTacToe.connection.send(command);
+  },
+
+  setStatus: function(status) {
+    $('status').set('text', status);
+  },
+
+  setGameStats: function() {
+    $('game-stats').set('text', 'Wins: '+this.wins+' / Losses: '+this.wins+' / Draws: '+this.draws);
+  },
+
+  setUserCount: function(userCount) {
+    $('user-count').set('text', 'Number of players: ' + userCount);
+  },
+
+  join: function(event) {
+    this.message('join');
+
+    this.setStatus('Connecting you to a game...');
+  },
+
+  reset: function() {
+    
   },
 
   onQueued: function(command) {
-    
+    this.setStatus('Waiting for another player...');
   },
 
   onStart: function(command) {
-    
+    this.setStatus('Game found! Their turn first...');
   },
 
   onTurn: function(command) {
-    
+    this.setStatus('Your turn...');
   },
 
   onMove: function(command) {
-    
+    $('cell-'+command.data.x+'-'+command.data.y).set('text', command.key);
+
+    this.setStatus('Their turn...');
+  },
+
+  move: function(x, y) {
+    this.message('move', {x: x, y: y});
   },
 
   onGameOver: function(command) {
-    
+    this.setStatus('Game over.');
+    this.setGameStats();
+    this.reset();
+  },
+
+  onWin: function(command) {
+    this.wins += 1;
+    this.setStatus('Game over. You win!');
+    this.setGameStats();
+  },
+
+  onLoss: function(command) {
+    this.losses += 1;
+    this.setStatus('Game over. You lose!');
+    this.setGameStats();
+  },
+
+  onDraw: function(command) {
+    this.draws += 1;
+    this.setStatus('Game over. It was a draw!');
+    this.setGameStats();
+  },
+
+  onUserCount: function(command) {
+    this.setUserCount(command.data);
   }
 });
 
-$('join-game').addEvent('click', function(event) {
-  currentGame = new TicTacToe();
+$$('.cell').addEvent('click', function(event) {
+  try {
+    currentGame.move($(this).get('id').split('-')[1], $(this).get('id').split('-')[2]);
+  } catch(error) {
+    alert('Please wait while we connect you to a game...');
+  }
 });
 
+currentGame = new TicTacToe();
