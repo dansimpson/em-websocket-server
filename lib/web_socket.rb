6e4e62ca52e06b36d85120dd8d6f885efbd4ee4f
Damir Zekic <damirz@gmail.com>
2010-06-12 18:24:37 +0200

    added support for new protocol version to Server (backwards compatibility is preserved)

--- lib/web_socket.rb
+++ lib/web_socket.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'eventmachine'
+require 'digest'
 require 'pp'
 
 module WebSocket
--- lib/web_socket/util.rb
+++ lib/web_socket/util.rb
@@ -1,17 +1,17 @@
 module WebSocket
 
-	class Util
+  class Util
 
-		@@path_regex    = /^GET (\/[^\s]*) HTTP\/1\.1$/
-		@@hs_regex      = /^HTTP\/1\.1 101 Web Socket Protocol Handshake$/
-		@@header_regex  = /^([^:]+):\s*([^$]+)/
+    @@path_regex    = /^GET (\/[^\s]*) HTTP\/1\.1$/
+    @@hs_regex      = /^HTTP\/1\.1 101 Web Socket Protocol Handshake$/
+    @@header_regex  = /^([^:]+):\s*([^$]+)/
 
-		# Parse http style headers into a ruby hash
-		def self.parse_headers data
-			lines = data.split("\r\n")
+    # Parse http style headers into a ruby hash
+    def self.parse_headers data
+      lines = data.split("\r\n")
       line  = lines.shift
       
-			headers = {}
+      headers = {}
       
       case line
       when @@path_regex
@@ -22,29 +22,34 @@ module WebSocket
         throw "Unrecognized Header!"
       end
       
+      while line = lines.shift
+        if line.chomp.empty?
+          break
+        end
+        
+        kvp = @@header_regex.match(line)
+        headers[kvp[1].strip.downcase.to_sym] = kvp[2].strip
+      end
       
-			lines.each do |line|
-				kvp = @@header_regex.match(line)
-				headers[kvp[1].strip.downcase.to_sym] = kvp[2].strip
-			end
+      challenge = lines.pop if lines
 
-			headers
-		end
+      [headers, challenge]
+    end
 
-		# encode ruby hash into HTTP style headers
-		def self.encode_headers data
-			result = ""
+    # encode ruby hash into HTTP style headers
+    def self.encode_headers data
+      result = ""
 
-			data.each_pair do |k,v|
-				result << k.to_s
-				result << ": "
-				result << v.to_s
-				result << "\r\n"
-			end
+      data.each_pair do |k,v|
+        result << k.to_s
+        result << ": "
+        result << v.to_s
+        result << "\r\n"
+      end
 
-			result << "\r\n"
-			result
-		end
-	  
+      result << "\r\n"
+      result
+    end
+    
   end
 end
